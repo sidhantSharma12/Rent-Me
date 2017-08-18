86109f7bc09d97947c06e3adfe1c57af67e579ef
Sidhant Sharma <sidhant0312@gmail.com>
2017-08-18 16:34:35 -0400

    using react app

--- README.md
+++
@@ -1,1 +0,0 @@
-Rent home appliances
--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
