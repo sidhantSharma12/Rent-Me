e4ab93ce26a914419fb80ad11495bb533544c392
Sidhant Sharma <sidhantsharma@Sidhants-MacBook-Pro.local>
2017-08-18 14:37:27 -0400

    initialize

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>UsSSSSSsscrs</h1>
+        <h1>Users</h1>
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
         )}
