1c0cbb1ff7b5c464f057e39a9839b3f7081d86b1
Sidhant Sharma <sidhant0312@gmail.com>
2017-08-23 20:25:29 -0400

    setup react router

--- rent/src/index.js
+++ rent/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+	<BrowserRouter>
+		<Route path="/" component={App}>
+		</Route>
+	</BrowserRouter>, 
+	document.getElementById('root')
+);
+
+
 registerServiceWorker();
